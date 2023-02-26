c73c593756c9d11a05cfc9e5efab4fdee4a5ea4d
emon-webdev <emon.webdev@gmail.com>
2023-02-26 08:50:09 +0600

    create file

---
+++ src/pages/Booking/Confirmation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Confirmation = () => {
+    return (
+        <div>
+            <h2>COnfirmation </h2>
+        </div>
+    );
+};
+
+export default Confirmation;
