6c10c1e4799c939393ae10e841525d106c3a1e79
Lingkon Hossain <lingkon@Lingkons-Mac-mini.local>
2023-03-07 02:16:45 +0600

    add mobile components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import { BsArrowRight } from 'react-icons/bs';
+import { FaPencilAlt } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 import CtaCard from "../../components/CtaCard";
 import PopupModal from "../../components/Modal/WlcModal";
 import ProgressBar from "../../components/ProgressBar";
@@ -9,8 +12,97 @@ import TopSeachForm from "../../components/TopSeachForm";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <PopupModal></PopupModal>
+      <Box 
+        sx={{
+          display:{md:"none", xs:"flex"},
+          background:"white",
+          borderBottom:"2px solid rgb(204, 206, 219)",
+          padding:"30px 15px",
+          alignItems:"center",
+          justifyContent:"space-between",
+          marginBottom:"25px"
+        }}
+      >
+        <Box>
+          <Box 
+            sx={{
+              display:"flex",
+              alignItems:"center",
+              marginBottom:"10px",
+              "& svg":{
+                marginLeft:"10px",
+                marginRight:"10px",
+              }
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize:"16px",
+                lineHeight:"normal",
+                fontWeight:"700",
+                color:"rgb(0, 3, 23)",
+                fontFamily:"'Public Sans', sans-serif",
+              }}
+            >
+              London
+            </Typography>
+            <BsArrowRight ></BsArrowRight>
+            <Typography
+              sx={{
+                fontSize:"16px",
+                lineHeight:"normal",
+                fontWeight:"700",
+                color:"rgb(0, 3, 23)",
+                fontFamily:"'Public Sans', sans-serif"
+              }}
+            >
+              Bangkok 
+            </Typography>
+          </Box>
+            <Typography 
+            sx={{
+              color:"rgba(7, 14, 57, 0.5)",
+              fontFamily:"'Public Sans', sans-serif",
+              fontWeight:"400",
+              fontSize:"14px",
+              lineHeight:"16px",
+              marginBottom:"4px"
+            }}
+            >Fri May 5 - Fri May 19</Typography>
+            <Typography 
+              component={"span"} 
+              className="nrPassengers"
+              sx={{
+                color:"rgba(7, 14, 57, 0.5)",
+                fontFamily:"'Public Sans', sans-serif",
+                fontWeight:"400",
+                fontSize:"14px",
+                lineHeight:"16px"
+              }}
+            >1 adult, Economy</Typography>
+        </Box>
+        <Typography
+          sx={{
+            display:"inline-block",
+            "& a":{
+              textDecoration:"none",
+              color:"#12172a"
+            },
+            "& a svg":{
+              fontSize:"24px",
+              display:"block",
+              marginBottom:"5px"
+            },
+          }}
+        >
+          <Link>
+            <FaPencilAlt></FaPencilAlt>
+            Edit 
+          </Link>
+        </Typography>
+      </Box>
       <TopSeachForm></TopSeachForm>
       <Box className="container">
         <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={3}>
@@ -32,7 +124,7 @@ const Home = () => {
         </Box>
       </Box>
 
-    </div>
+    </>
   );
 };
 
