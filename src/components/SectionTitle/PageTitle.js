ea702f26e219bc830eaca31d2a1807e384c498ac
lingkonbd <kevin.iubd@gmail.com>
2023-03-07 22:04:40 +0600

    fixing mobile card

--- src/components/SectionTitle/PageTitle.js
+++ src/components/SectionTitle/PageTitle.js
@@ -9,7 +9,7 @@ const PageTitle = ({ title }) => {
           fontFamily: "'Public Sans', sans-serif !important",
           fontSize: { md: "36px", xs: "28px" },
           lineHeight: "28px",
-          paddingLeft: "0px",
+          paddingLeft: {md:"0px", xs:"15px"},
           fontWeight: "700",
           color: "rgb(0, 3, 23)",
           margin: { md: "65px 0 40px", xs: "35px 0 20px" },
