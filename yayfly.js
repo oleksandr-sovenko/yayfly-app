2b819c3deb116d83f0f43d9f95a33e1dd69035af
Oleksandr Sovenko <info@oleksandrsovenko.com>
2023-03-13 10:29:41 +0000

    Work on WP plugin

--- yayfly.js
+++ yayfly.js
@@ -1,111 +1,338 @@
+window.airports = [];
+window.travelers = {
+	adults: 1,
+	children: 0,
+	infants: 0,
+	cabinClass: 'economy',
+};
+
+
+const cabinClasses = {
+	'economy': 'Economy',
+	'premium-economy': 'Premium Economy',
+	'business': 'Business',
+	'first-class': 'First Class',
+};
+
+
 jQuery(document).ready(($) => {
 	const searchForm = $('form.searchForm');
 
 	if (searchForm.length) {
-		const whereFrom = searchForm.find('[placeholder="Where from?"]'),
-			  whereTo   = searchForm.find('[placeholder="Where to?"]'),
-			  dates     = searchForm.find('[placeholder="Dates"]'),
+		const dates     = searchForm.find('[placeholder="Dates"]'),
 			  travelers = searchForm.find('[placeholder="Who are the travelers?"]');
 
+
+		setInterval(() => {
+			if (window.device.current === 'mobile') {
+				if ($('.yayfly-dropdown-list.show').length || $('.daterangepicker:visible').length) {
+					$('body').css({ overflow: 'hidden' });
+				} else {
+					$('body').css({ overflow: 'auto' });
+				}
+			}
+		}, 1000);
+
+
+		searchForm.attr('data-tab', 'round-trip');
+
+
+		/**
+		 * 
+		 */
+		window.device = { current: '', before: '' };
+
+
+		/**
+		 * 
+		 */
+		$(window).resize(() => {
+			window.device.before = window.device.current;
+
+			if ($(window).width() > 767)
+				window.device.current = 'desktop';
+			else
+				window.device.current = 'mobile';
+
+			if (window.device.current !== window.device.before) {
+				if (window.device.current === 'mobile') {
+					searchForm.find('[name="whereFrom"]').attr('readonly', true);
+					searchForm.find('[name="whereTo"]').attr('readonly', true);
+				} else {
+					searchForm.find('[name="whereFrom"]').attr('readonly', false);
+					searchForm.find('[name="whereTo"]').attr('readonly', false);
+				}
+
+				dates.daterangepicker({
+					locale: { format: 'ddd MMM D' },    		
+					autoApply: window.device.current === 'desktop' ? true : false,
+				}, function(start, end, label) {
+					console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+				});				
+			}
+		});
+
+
+		/**
+		 * 
+		 */
+		$(window).trigger('resize');
+
+
+		/**
+		 * 
+		 */
+		const updateTravelers = () => {
+			let result = [];
+
+			if (window.travelers.adults)
+				result.push(`${window.travelers.adults} ${window.travelers.adults > 1 ? 'Adults' : 'Adult'}`);
+
+			if (window.travelers.children)
+				result.push(`${window.travelers.children} ${window.travelers.children > 1 ? 'Children' : 'Child'}`);
+
+			if (window.travelers.infants)
+				result.push(`${window.travelers.infants} ${window.travelers.infants > 1 ? 'Infants' : 'Infant'}`);
+
+			result.push(`${cabinClasses[window.travelers.cabinClass]}`);
+
+			searchForm.find('[name="travelers"]').val(result.join(', '));
+		}
+
+
+		updateTravelers();
+
+
+		/**
+		 * 
+		 */
+		const search = (value) => {
+			let items = [];
+
+			if (value.length >= 3) {
+				for (var airport of window.airports) {
+					if (new RegExp(value, 'i').test(airport.text))
+                        items.push(airport);
+				}
+
+				if (!items.length) {
+					items.push({ value: 'null', text: 'Not Found' });
+				}				
+			}			
+
+			return items;
+		};
+
+
+		/**
+		 * 
+		 */
+		const liveSearch = (e) => {
+			const value  = e.target.value,
+				  parent = $(e.target).parent();
+
+			let items = [];
+
+			let icon = '/wp-content/plugins/yayfly/images/plane-departure.svg';
+
+			if (e.target.name === 'whereTo')
+				icon = '/wp-content/plugins/yayfly/images/plane-arrival.svg';
+
+			if (window.device.current === 'desktop' && value.length >= 3)
+				items = search(value);
+
+			if ((window.device.current === 'desktop' && value.length >= 3) || window.device.current === 'mobile') {
+				parent.find('.yayfly-dropdown-list').remove();
+				parent.append(`
+					<div class="yayfly-dropdown-list show">
+						<input value="${value}" placeholder="Please type at least 3 letters">
+						<ul>
+							${items.map((item) => {
+								return `<li data-value="${item.value}">
+									<img src="${icon}">
+									<span>${item.text}</span>
+								</li>`;
+							}).join('')}
+						</ul>
+						<button class="done">Done</button>
+					</div>
+				`);
+
+				parent.find('.yayfly-dropdown-list input').on('input', (e) => {
+					const value = e.target.value;
+
+					let items = search(value);
+
+					parent.find('.yayfly-dropdown-list ul').html(`
+						${items.map((item) => {
+							return `<li data-value="${item.value}">
+								<img src="${icon}">
+								<span>${item.text}</span>
+							</li>`;
+						}).join('')}
+					`);
+				});
+
+				if (window.device.current === 'mobile')
+					parent.find('.yayfly-dropdown-list input').trigger('input').focus();
+			}
+		} 
+
+
+		/**
+		 * 
+		 */
+		searchForm.on('input', '[name="whereFrom"], [name="whereTo"]', liveSearch);
+
+
+		/**
+		 * 
+		 */
+		searchForm.on('click', '.error', (e) => {
+			const el = $(e.target);
+
+			el.removeClass('error');
+		});
+
+
+		/**
+		 * 
+		 */
+		searchForm.on('click', '[name="whereFrom"],[name="whereTo"],[name="travelers"]', (e) => {
+			const el     = $(e.target),
+				  parent = el.parent();
+
+			if (el.attr('name') === 'travelers') {
+				parent.find('.yayfly-dropdown-list').remove();
+				parent.append(`
+					<div class="yayfly-dropdown-list show yayfly-p15">
+						<div class="yayfly-row">
+							<div class="yayfly-col yayfly-w50">
+								<h5>Adults</h5>
+								+ 12 years old
+							</div>
+							<div class="yayfly-col yayfly-w50 yayfly-text-right">
+								<button class="minus">-</button>
+								<span class="count" data-type="adults">${window.travelers.adults}</span>
+								<button class="plus">+</button>
+							</div>
+						</div>
+						<div class="yayfly-row">
+							<div class="yayfly-col yayfly-w50">
+								<h5>Children</h5>
+								Between 2 and 11 years old included
+							</div>
+
+							<div class="yayfly-col yayfly-w50 yayfly-text-right">
+								<button class="minus">-</button>
+								<span class="count" data-type="children">${window.travelers.children}</span>
+								<button class="plus">+</button>
+							</div>
+						</div>
+						<div class="yayfly-row">
+							<div class="yayfly-col yayfly-w50">
+								<h5>Infants</h5>
+								- 2 years old
+							</div>
+							<div class="yayfly-col yayfly-w50 yayfly-text-right">
+								<button class="minus">-</button>
+								<span class="count" data-type="infants">${window.travelers.infants}</span>
+								<button class="plus">+</button>
+							</div>
+						</div>
+						<div class="yayfly-row">
+							<div class="yayfly-col yayfly-w100 yayfly-cabin-class">
+								<span>Cabin class</span>
+								<select>
+									${Object.keys(cabinClasses).map((name) => {
+										return `<option ${window.travelers.cabinClass == name ? 'selected' : ''} value="${name}">${cabinClasses[name]}</option>`;
+									})}
+								</select>
+							</div>
+						</div>
+
+						<button class="done">Done</button>
+					</div>
+				`);
+			} else {
+				el.trigger('input');
+			}
+		});
+
+
+		/**
+		 * 
+		 */
+		dates.attr('readonly', true);
+		travelers.attr('readonly', true);
+
+
+		/**
+		 * 
+		 */
+		$.get('/api/airports', (airports) => {
+			window.airports = airports;
+		});
+
+
+		/**
+		 * 
+		 */
 		travelers.on('keypress', (e) => {
 			return false;
 		})
-			  
-		whereFrom.parent().css({ position: 'relative' });
-
-		whereFrom.parent().append(`
-			<div class="yayfly-dropdown-list">
-				<ul>
-					<li>Item 1</li>
-					<li>Item 2</li>
-					<li>Item 3</li>
-					<li>Item 4</li>
-					<li>Item 5</li>
-					<li>Item 6</li>
-				</ul>
-			</div>
-		`);
 
 
 		/**
 		 * 
 		 */
-		whereTo.parent().append(`
-			<div class="yayfly-dropdown-list">
-				<ul>
-					<li>Item 11</li>
-					<li>Item 12</li>
-					<li>Item 13</li>
-					<li>Item 14</li>
-					<li>Item 15</li>
-					<li>Item 16</li>
-				</ul>
-			</div>
-		`);
+		$(document).on('click', '.yayfly-dropdown-list .done', (e) => {
+			const el       = $(e.target),
+				  dropdown = el.closest('.yayfly-dropdown-list');
+
+			dropdown.removeClass('show');
+		});
 
 
 		/**
 		 * 
 		 */
-		travelers.parent().append(`
-			<div class="yayfly-dropdown-list yayfly-p15">
-				<div class="yayfly-row">
-					<div class="yayfly-col yayfly-w50">
-						<h5>Adults</h5>
-						+ 12 years old
-					</div>
-					<div class="yayfly-col yayfly-w50 yayfly-text-right">
-						<button class="minus">-</button>
-						<span class="count">0</span>
-						<button class="plus">+</button>
-					</div>
-				</div>
-				<div class="yayfly-row">
-					<div class="yayfly-col yayfly-w50">
-						<h5>Children</h5>
-						Between 2 and 11 years old included
-					</div>
-
-					<div class="yayfly-col yayfly-w50 yayfly-text-right">
-						<button class="minus">-</button>
-						<span class="count">0</span>
-						<button class="plus">+</button>
-					</div>
-				</div>
-				<div class="yayfly-row">
-					<div class="yayfly-col yayfly-w50">
-						<h5>Infants</h5>
-						- 2 years old
-					</div>
-					<div class="yayfly-col yayfly-w50 yayfly-text-right">
-						<button class="minus">-</button>
-						<span class="count">0</span>
-						<button class="plus">+</button>
-					</div>
-				</div>
-			</div>
-		`);
-
-
-		dates.daterangepicker({
-    		"startDate": "03/03/2023",
-    		"endDate": "03/09/2023",
-    		autoApply: true,
-		}, function(start, end, label) {
-  			console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
-		});		
-
-
 		$(document).on('click', '.yayfly-dropdown-list li', (e) => {
-			const el = $(e.target);
+			let el     = $(e.target),
+				parent = el.closest('.yayfly-dropdown-list').parent();
+
+			if (e.target.tagName !== 'LI')
+				el = el.closest('li');
+
+			parent.find('input').val(el.attr('data-value'));
 
 			el.closest('.yayfly-dropdown-list').removeClass('show');
 		});
 
 
+		/**
+		 * 
+		 */
+		$(document).on('change', '.yayfly-dropdown-list select', (e) => {
+			const el    = $(e.target),
+				  value = el.val();
+
+			window.travelers.cabinClass = value;
+
+			updateTravelers();
+		});
+
+
+		/**
+		 * 
+		 */
 		$(document).on('click', '.yayfly-dropdown-list button', (e) => {
 			const el      = $(e.target),
-				  elCount = el.parent().find('.count');
+				  elCount = el.parent().find('.count'),
+				  type    = elCount.data('type');
+
+			if (el.hasClass('done'))
+				return;
 
 			let count = parseInt(elCount.text());
 
@@ -124,7 +351,26 @@ jQuery(document).ready(($) => {
 			if (count > 7)
 				count = 7;
 
+			if (type == 'adults') {
+				if (count < 1)
+					count = 1;
+
+				if (window.travelers.infants > count) {
+					window.travelers.infants = count;
+					$('.count[data-type="infants"]').text(count);
+				}
+			}
+
+			if (type == 'infants') {
+				if (count > window.travelers.adults)
+					count = window.travelers.adults;
+			}
+
+			window.travelers[type] = count;
+
 			elCount.text(count);
+
+			updateTravelers();
 		});
 
 
@@ -132,69 +378,147 @@ jQuery(document).ready(($) => {
 		 * 
 		 */
 		$(document).on('click', (e) => {
+			if (window.device.current === 'mobile')
+				return;
+
 			const el = $(e.target);
 
 			if (el.closest('.yayfly-dropdown-list').length)
 				return;
 
-			searchForm.find('.yayfly-dropdown-list').removeClass('show');			
+			searchForm.find('.yayfly-dropdown-list').removeClass('show');
 
-			if (el[0] === whereFrom[1]) {
-				// console.log('adasd');
-				whereFrom.parent().find('.yayfly-dropdown-list').addClass('show');
-			}
+			if ((e.target.name === 'whereFrom' || e.target.name === 'whereTo') && e.target.value.length >=3)
+				$(e.target).parent().find('.yayfly-dropdown-list').addClass('show');
 
-			if (el[0] === whereTo[0]) {
-				whereTo.parent().find('.yayfly-dropdown-list').addClass('show');
-			}
+			if (e.target.name === 'travelers')
+				$(e.target).parent().find('.yayfly-dropdown-list').addClass('show');
 
-			if (el[0] === travelers[0]) {
-				travelers.parent().find('.yayfly-dropdown-list').addClass('show');
-			}
+			// if (el[0] === travelers[0]) {
+			// 	travelers.parent().find('.yayfly-dropdown-list').addClass('show');
+			// }
+		});	
+
+
+		/**
+		 * 
+		 */
+		searchForm.on('click', '.initialSearch', (e) => {
+			searchForm.removeClass('collapsed');
 		});
 
 
 		/**
 		 * 
 		 */
-		$(searchForm).on('click', '.formTab button', (e) => {
+		searchForm.on('click', '.formTab button', (e) => {
 			e.preventDefault();
 
 			const el = $(e.target);
 
+			if (window.device.current === 'desktop')
+				searchForm.removeClass('collapsed');
+
+			searchForm.attr('data-tab', el.attr('name'));
+
 			searchForm.find('.formTab button').removeClass('active');
 			el.addClass('active');
+
+			searchForm.find('.addFlightBtn').css({ display: 'none' });
+
+			if (el.attr('name') == 'multi-city') {
+				searchForm.find('.addFlightBtn').css({ display: 'flex' });
+			} else {
+				$(searchForm).find('.wrapperItem:not(:first)').remove();
+			}
+
+			searchForm.find('[name="dates"]').prop('placeholder', 'Dates');
+
+			if (el.attr('name') == 'one-way' || el.attr('name') == 'multi-city') {
+				searchForm.find('[name="dates"]').prop('placeholder', 'Date');
+
+				dates.daterangepicker({
+					singleDatePicker: true,
+					locale: { format: 'ddd MMM D' },    		
+					autoApply: window.device.current === 'desktop' ? true : false,
+				}, function(start, end, label) {
+					// console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+				});
+			} else {
+				dates.daterangepicker({
+					locale: { format: 'ddd MMM D' },    		
+					autoApply: window.device.current === 'desktop' ? true : false,
+				}, function(start, end, label) {
+					// console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+				});
+			}
+
+			searchForm.find('.error').removeClass('error');
 		});
 
 
 		/**
 		 * 
 		 */
-		$(searchForm).on('click', '.addFlightBtn', (e) => {
-			console.log('addFlightBtn')
+		searchForm.on('click', '.addFlightBtn', (e) => {
+			searchForm.find('.error').removeClass('error');
+
+			const wrapper = searchForm.find('.wrapper'),
+				  item    = searchForm.find('.wrapperItem:eq(0)').clone();
+
+			item.find('input').val('');
+			item.find('[name="dates"]').daterangepicker({
+				singleDatePicker: true,
+				locale: { format: 'ddd MMM D' },    		
+				autoApply: window.device.current === 'desktop' ? true : false,
+			}, function(start, end, label) {
+
+			});
+
+			wrapper.append(item);
+
+			updateTravelers();
 		});
 
 
 		/**
 		 * 
 		 */
-		$(searchForm).on('click', '[type="submit"]', (e) => {
-			location.href = '/search';
+		searchForm.on('click', '[type="submit"]', (e) => {
+			const tab = searchForm.attr('data-tab');
+
+			for (const name of ['whereFrom', 'whereTo', 'dates', 'travelers']) {
+				const el = searchForm.find(`[name="${name}"]`);
+
+				if (!el.val().length) {
+					console.log(`${name} is empty`);
+					el.addClass('error');
+				}
+			}
+
+			if (!searchForm.find('.error').length)
+				location.href = '/search';
 		});
 
 
 		/**
 		 * 
 		 */
-		$(searchForm).on('click', 'img.iconimg', (e) => {
-			console.log('swap')
+		searchForm.on('click', 'img.iconimg', (e) => {
+			const el   = $(e.target),
+				  from = el.closest('.inputRow').find('[name="whereFrom"]'),
+				  to   = el.closest('.inputRow').find('[name="whereTo"]'),
+				  temp = from.val();
+			
+			from.val(to.val());
+			to.val(temp);
 		});
 
 
 		/**
 		 * 
 		 */
-		$(searchForm).on('submit', (e) => {
+		searchForm.on('submit', (e) => {
 			e.preventDefault(e);
 		});
 	}
--- yayfly.php
+++ yayfly.php
@@ -26,7 +26,7 @@ add_action('wp_enqueue_scripts', function() {
 	wp_register_script('daterangepicker', 'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js', array('jquery', 'moment'));
 	wp_enqueue_script('daterangepicker');	
 
-	wp_register_style('yayfly', plugin_dir_url(__FILE__).'yayfly.css', false);
+	wp_register_style('yayfly', plugin_dir_url(__FILE__).'yayfly.css', false, time());
 	wp_enqueue_style('yayfly');
 	wp_register_script('yayfly', plugin_dir_url(__FILE__).'yayfly.js', NULL, time(), true);
 	wp_enqueue_script('yayfly');
@@ -38,7 +38,7 @@ add_action('wp_enqueue_scripts', function() {
  */
 add_action('init', function() {
 	if (
-		$_SERVER['REQUEST_URI'] == '/search' or
+		strstr($_SERVER['REQUEST_URI'], '/search') or
 		$_SERVER['REQUEST_URI'] == '/booking-details' or
 		$_SERVER['REQUEST_URI'] == '/confirm-booking' or
 		$_SERVER['REQUEST_URI'] == '/payment'		
@@ -47,14 +47,276 @@ add_action('init', function() {
 		$content = file_get_contents(__DIR__.'/build/index.html');
 		$content = str_replace([
 			'href="/',
-			'src="/static/'
+			// 'src="/static/'
 		], [
 			'href="'.$path.'/',
-			'src="'.$path.'/static/'
+			// 'src="'.$path.'/static/'
 		], $content);
 
 		die($content);
 	}
+
+
+	if (strstr($_SERVER['REQUEST_URI'], '/api/')) {
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+		header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With');
+		header('Content-Type: application/json');
+
+
+		// /api/airports
+		if ($_SERVER['REQUEST_URI'] == '/api/airports') {
+			die(file_get_contents(__DIR__.'/airports.json'));
+			// $input = json_decode(file_get_contents('php://input'));
+			// // wp_send_json($input);
+
+			// $data = [ 'data' => [
+			// 	'type' => 'instant',
+			// 	'services' => $input->services,
+			// 	'selected_offers' => [$input->offer->id],
+			// 	'payments' => [
+			// 		['type' => 'balance', 'currency' => 'USD', 'amount' => round($input->amount, 2)]
+			// 	],
+			// 	'passengers' => $input->passengers,					
+			// ] ];
+
+			// $data = duffel_request('https://api.duffel.com/air/orders', $data);
+
+			// wp_send_json($data);
+		}		
+
+
+		// /api/order/create
+		if ($_SERVER['REQUEST_URI'] == '/api/order/create') {
+			$input = json_decode(file_get_contents('php://input'));
+			// wp_send_json($input);
+
+			$data = [ 'data' => [
+				'type' => 'instant',
+				'services' => $input->services,
+				'selected_offers' => [$input->offer->id],
+				'payments' => [
+					['type' => 'balance', 'currency' => 'USD', 'amount' => round($input->amount, 2)]
+				],
+				'passengers' => $input->passengers,					
+			] ];
+
+			$data = duffel_request('https://api.duffel.com/air/orders', $data);
+
+			wp_send_json($data);
+		}		
+
+
+		// /api/payments/intent/confirm
+		if (strstr($_SERVER['REQUEST_URI'], '/api/payments/intent/confirm')) {
+			$id    = preg_replace('@.*\/@', '', $_SERVER['REQUEST_URI']);
+			$input = json_decode(file_get_contents('php://input'));
+
+			if (empty($id))
+				wp_send_json(['message' => 'Failure', 'id' => $id]);
+
+			$data = duffel_request(
+				'https://api.duffel.com/payments/payment_intents/'.$id.'/actions/confirm',
+				['data' => ['id' => $id]]
+			);
+
+			wp_send_json($data);
+		}	
+
+
+		// /api/payments/refund
+		if ($_SERVER['REQUEST_URI'] == '/api/payments/refund') {
+			$input = json_decode(file_get_contents('php://input'));
+
+			$data = duffel_request(
+				'https://api.duffel.com/payments/refunds',
+				[ 'data' => [
+					'payment_intent_id' => $input->id,
+					'currency'          => 'USD',
+					'amount'            => $input->amount,
+				]]
+			);
+
+			wp_send_json($data);
+		}
+
+
+		// /api/payments/intent
+		if (strstr($_SERVER['REQUEST_URI'], '/api/payments/intent')) {
+			$currency = !empty($_GET['currency']) ? $_GET['currency']         : 'USD';
+			$amount   = !empty($_GET['amount'])   ? floatval($_GET['amount']) : 0;
+
+			if (empty($amount))
+				wp_send_json(['message' => 'Failure', 'data' => NULL]);			
+
+			$data = duffel_request(
+				'https://api.duffel.com/payments/payment_intents',
+				[ 'data' => [ 'amount' => sprintf('%.2f', $amount), 'currency' => $currency ] ]
+			);
+
+			wp_send_json($data);
+		}
+
+
+		// /api/offers
+		if ($_SERVER['REQUEST_URI'] == '/api/offers') {
+			wp_send_json(json_decode(file_get_contents('/tmp/example.json')));
+
+			$input = json_decode(file_get_contents('php://input'), true);
+			if (empty($input))
+				$input = [];
+
+			$required_fields = [];
+
+			if (empty($input['type']))
+				$input['type'] = 'round';
+
+			foreach(['from', 'to', 'depart', 'return'] as $field) {
+				if (empty($input[$field])) {
+					if ($field == 'return' and $input['type'] == 'one')
+						continue;
+
+					$required_fields[] = $field;
+				}
+			}
+
+			if (!empty($required_fields))
+				wp_send_json([]);
+
+			$data = duffel_offer_requests([
+				'class'      => !empty($input['class'])      ? $input['class']      : '',
+				'type'       => !empty($input['type'])       ? $input['type']       : '',
+				'from'       => !empty($input['from'])       ? $input['from']       : '',
+				'to'         => !empty($input['to'])         ? $input['to']         : '',
+				'depart'     => !empty($input['depart'])     ? $input['depart']     : '',
+				'return'     => !empty($input['return'])     ? $input['return']     : '',
+				'passengers' => !empty($input['passengers']) ? $input['passengers'] : '',
+			]);
+
+			if (!$data)
+				wp_send_json($data);
+			
+			$data = duffel_request('https://api.duffel.com/air/offer_requests/'.$data->id);
+
+			file_put_contents('/tmp/example.json', json_encode($data));
+			// wp_send_json(json_decode(file_get_contents('/tmp/example.json')));
+
+			wp_send_json($data);
+		}
+
+
+		// /api/offer/:id
+		if (strstr($_SERVER['REQUEST_URI'], '/api/offer/')) {
+			$id   = preg_replace('@.*\/@', '', $_SERVER['REQUEST_URI']);	
+			$data = duffel_request('https://api.duffel.com/air/offers/'.$id.'?return_available_services=true');
+			if (!empty($data->data))
+				$data->data->seatmaps = duffel_request('https://api.duffel.com/air/seat_maps?offer_id='.$id);
+			wp_send_json($data);
+		}		
+
+		wp_send_json([]);
+	}	
 });
+
+
+/**
+ * 
+ */
+function duffel_request($url, $data = []) {
+	$headers = array (
+		'Authorization: Bearer duffel_test_3O7Bl3sP-YmxCXxWK0Is--nLVqs9i91-K8cm53cy89O',
+		'Accept: application/json',
+		'Content-Type: application/json',
+		'Duffel-Version: v1',
+	);
+ 
+  	$curl = curl_init();
+  	curl_setopt($curl, CURLOPT_URL, $url);
+  	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+  	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);    
+ 
+  	if (!empty($data))
+    	curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+ 
+  	$response = curl_exec($curl);
+ 
+  	if (curl_errno($curl))
+    	return curl_errno($curl);
+ 
+  	curl_close($curl);
+
+  	$response = json_decode($response);
+ 
+  	return $response;
+}
+
+
+/**
+ * @type
+ * @from
+ * @to
+ * @depart
+ * @return
+ */
+function duffel_offer_requests($params) {
+	$passengers = [];
+
+	foreach(['class', 'type', 'from', 'to', 'depart', 'return', 'passengers'] as $name) {
+		if ($name == 'class' and empty($params[$name]))
+			$params[$name] = 'economy';
+
+		if ($name == 'type' and empty($params[$name]))
+			$params[$name] = 'one';
+
+		if ($name == 'passengers' and empty($params[$name]))
+			$params[$name] = ['adult' => 1];
+
+		if ($name == 'return' and $params['type'] == 'one')
+			continue;
+
+		if (empty($params[$name]))
+			return false;
+	}
+
+	foreach(['adult', 'child', 'infant'] as $type) {
+		if ($type == 'infant')
+			$type = 'infant_without_seat';
+
+		if (!empty($params['passengers'][$type])) {
+			for ($i = 0; $i < intval($params['passengers'][$type]); $i++)
+				$passengers[] = ['type' => $type];
+		}
+	}
+
+	$data = [
+		'slices' => [
+			[
+				'origin' => $params['from'],
+				'destination' => $params['to'],
+				'departure_date' => $params['depart'],
+			],
+		],
+		'passengers'  => $passengers,
+		'cabin_class' =>$params['class']		
+	];
+
+	if ($params['type'] == 'round')
+		$data['slices'][] = [
+			'origin' => $params['to'],
+			'destination' => $params['from'],
+			'departure_date' => $params['return'],
+		];
+
+	$json = duffel_request(
+		'https://api.duffel.com/air/offer_requests?supplier_timeout=10000',
+		[ 'data' => $data ]
+	);
+
+	if (!empty($json->data)) {
+		return $json->data;
+	} else
+		return [];
+}
 
 ?>
