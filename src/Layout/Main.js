2cf86a9ffbeccb544cc4616c7f8a712f1b8058c3
Oleksandr Sovenko <info@oleksandrsovenko.com>
2023-03-15 13:44:03 +0200

    Work on search fields

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -3,14 +3,18 @@ import { Outlet } from "react-router";
 import Footer from "../pages/Shared/Footer";
 import Header from "../pages/Shared/Header";
 
+
+if (window.location.pathname === '/')
+    window.location.reload(true);
+
 const Main = () => {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    );
 };
 
 export default Main;
