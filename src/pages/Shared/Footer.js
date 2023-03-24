1673cf022ef91c5a45779f4966f4c243af812702
Oleksandr Sovenko <info@oleksandrsovenko.com>
2023-03-25 00:36:56 +0200

    Fixes ...

--- src/pages/Shared/Footer.js
+++ src/pages/Shared/Footer.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { FaPhoneAlt, FaAngleUp } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { Grid } from "@mui/material";
 import logo from "../../assets/yayfly.svg";
 
 
@@ -20,32 +21,49 @@ const Footer = () => {
                 <FaAngleUp />
             </div>
             <footer className='footer'>
-                <div className="container grid grid-column align-item">
-                    <div className="logo">
-                        <Link to="#" onClick={((e) => {
-                            e.preventDefault();
+                <div className="container">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sx={{ textAlign: { xs: 'center' }, marginTop: '15px', marginBottom: '15px', display: { xs: 'block', md: 'none' } }}>
+                            <Link to="#" onClick={((e) => { e.preventDefault(); window.location.href = window.flights_engine.url })}>
+                                <img src={logo} alt="yayFly" />
+                            </Link>                        
+                        </Grid>
+                    </Grid>
+                
+                    <Grid container spacing={2}>
+                        <Grid item md={6} xs={12}>
+                            Traveling is a wonderful experience, and we want you to have peace of mind when booking your tickets. We provide the best and the cheapest flight options without any advertisements or hidden fees, and our customer service is excellent. Our goal is to make your travel experience effortless and stress-free.
+                        </Grid>
+                        <Grid item md={3} xs={12} sx={{ paddingLeft: { md: "75px !important", xs: "0" } }}>
+                            <h2>HELPFUL TOOLS</h2>
+                            <ul>
+                                <li><a href="https://yayfly.com/contact-us/">CONTACT US</a></li>
+                                <li><a href="https://yayfly.com/airline-baggage-fees/">AIRLINE BAGGAGE POLICY</a></li>
+                                <li><a href="https://yayfly.com/airline-telephone-numbers/">AIRLINE TELEPHONE NUMBERS</a></li>
+                            </ul>
+                        </Grid>
+                        <Grid item md={3} xs={12} sx={{ paddingLeft: { md: "75px !important", xs: "0" } }}>
+                            <h2>LEGAL</h2>
+                            <ul>
+                                <li><a href="https://yayfly.com/terms-and-conditions/">TERMS & CONDITIONS</a></li>
+                                <li><a href="https://yayfly.com/privacy-policy/">PRIVACY POLICY</a></li>
+                                <li><a href="https://yayfly.com/refund-cancellation/">REFUND & CANCELLATION</a></li>
+                                <li><a href="https://yayfly.com/accessibility-contract/">ACCESSIBILITY POLICY</a></li>
+                                <li><a href="https://yayfly.com/taxes-and-fees/">TAXES AND FEES</a></li>
+                            </ul>
+                        </Grid>
+                    </Grid>
 
-                            window.location.href = window.flights_engine.url;
-                        })}>
-                            {" "}
-                            <img src={logo} alt="yayFly" />
-                        </Link>
-                        <div>
-                            <span>EXCLUSIVE <br/>PHONE DEALS</span>
-                            <a href="tel:8882112111"><span><FaPhoneAlt /></span></a>
-                        </div>
-                    </div>
-                    <div className="header-right">
-                        <div>
-                            <p>
-                                <strong>Save even more.</strong> Talk to our booking expert now.
-                            </p>
-                            <span>Agent is available now</span>
-                        </div>
-                        <a href="tel:8882112111">
-                            <FaPhoneAlt /> (888) 211.2111
-                        </a>
-                    </div>
+                    <Grid container spacing={2}>
+                        <Grid item md={2} xs={12} sx={{ textAlign: { xs: 'center', md: 'left' } }}>
+                            <Link to="#" onClick={((e) => { e.preventDefault(); window.location.href = window.flights_engine.url })}>
+                                <img src={logo} alt="yayFly" />
+                            </Link>                        
+                        </Grid>
+                        <Grid item md={10} xs={12} sx={{ textAlign: { xs: 'center', md: 'left' }, position: { xs: 'inherit', md: 'relative' }, paddingTop: { xs: '0', md: '30px !important' } }}>
+                            <span>COPYRIGHT © 2023 YAY FLY, INC. ALL RIGHTS RESERVED.</span>
+                        </Grid>
+                    </Grid>
                 </div>
             </footer>
         </>
